5088dd7247014982797501b1a6eb100e33142f8d
Sebastian Ślimak <sebastian.slimak707@gmail.com>
2022-10-26 14:55:46 +0200

    apperrors

---
+++ backend/lib/apperrors/apperrors.go
@@ -0,0 +1,126 @@
+package apperrors
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+type Type string
+
+const (
+	Authorization        Type = "AUTHORIZATION"          // Authentication Failures -
+	BadRequest           Type = "BAD_REQUEST"            // Validation errors / BadInput
+	Forbidden            Type = "FORBIDDEN"              // Forbidden action
+	Conflict             Type = "CONFLICT"               // Already exists (eg, create account with existent email) - 409
+	Internal             Type = "INTERNAL"               // Server (500) and fallback errors
+	NotFound             Type = "NOT_FOUND"              // For not finding resource
+	PayloadTooLarge      Type = "PAYLOAD_TOO_LARGE"      // for uploading tons of JSON, or an image over the limit - 413
+	ServiceUnavailable   Type = "SERVICE_UNAVAILABLE"    // For long running handlers
+	UnsupportedMediaType Type = "UNSUPPORTED_MEDIA_TYPE" // for http 415
+)
+
+type Error struct {
+	Type    Type   `json:"type"`
+	Message string `json:"message"`
+}
+
+func (e *Error) Error() string {
+	return e.Message
+}
+
+func (e *Error) Status() int {
+	switch e.Type {
+	case Authorization:
+		return http.StatusUnauthorized
+	case BadRequest:
+		return http.StatusBadRequest
+	case Forbidden:
+		return http.StatusForbidden
+	case Conflict:
+		return http.StatusConflict
+	case Internal:
+		return http.StatusInternalServerError
+	case NotFound:
+		return http.StatusNotFound
+	case PayloadTooLarge:
+		return http.StatusRequestEntityTooLarge
+	case ServiceUnavailable:
+		return http.StatusServiceUnavailable
+	case UnsupportedMediaType:
+		return http.StatusUnsupportedMediaType
+	default:
+		return http.StatusInternalServerError
+	}
+}
+
+func Status(err error) int {
+	var e *Error
+	if errors.As(err, &e) {
+		return e.Status()
+	}
+	return http.StatusInternalServerError
+}
+
+func NewAuthorization(reason string) *Error {
+	return &Error{
+		Type:    Authorization,
+		Message: reason,
+	}
+}
+
+func NewBadRequest(reason string) *Error {
+	return &Error{
+		Type:    BadRequest,
+		Message: fmt.Sprintf("Bad request. Reason: %v", reason),
+	}
+}
+
+func NewForbidden(reason string) *Error {
+	return &Error{
+		Type:    Forbidden,
+		Message: fmt.Sprintf("Forbidden action. Reason: %v", reason),
+	}
+}
+
+func NewConflict(name string, value string) *Error {
+	return &Error{
+		Type:    Conflict,
+		Message: fmt.Sprintf("resource: %v with value: %v already exists", name, value),
+	}
+}
+
+func NewInternal() *Error {
+	return &Error{
+		Type:    Internal,
+		Message: fmt.Sprintf("Internal server error."),
+	}
+}
+
+func NewNotFound(name string, value string) *Error {
+	return &Error{
+		Type:    NotFound,
+		Message: fmt.Sprintf("resource: %v with value: %v not found", name, value),
+	}
+}
+
+func NewPayloadTooLarge(maxBodySize int64, contentLength int64) *Error {
+	return &Error{
+		Type:    PayloadTooLarge,
+		Message: fmt.Sprintf("Max payload size of %v exceeded. Actual payload size: %v", maxBodySize, contentLength),
+	}
+}
+
+func NewServiceUnavailable() *Error {
+	return &Error{
+		Type:    ServiceUnavailable,
+		Message: fmt.Sprintf("Service unavailable or timed out"),
+	}
+}
+
+func NewUnsupportedMediaType(reason string) *Error {
+	return &Error{
+		Type:    UnsupportedMediaType,
+		Message: reason,
+	}
+}
