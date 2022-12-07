0b427497511ceeaee3a2e46a26ee860085c05d39
Sebastian Ślimak <sebastian.slimak707@gmail.com>
2022-12-07 18:43:54 +0100

    Member events

--- backend/lib/events/member_deleted.go
+++ backend/lib/events/member_deleted.go
@@ -7,6 +7,7 @@ import (
 type MemberDeletedEvent struct {
 	ID      uuid.UUID `json:"ID"`
 	GroupID uuid.UUID `json:"groupID"`
+	UserID  uuid.UUID `json:"userID"`
 }
 
 func (MemberDeletedEvent) EventName() string {
