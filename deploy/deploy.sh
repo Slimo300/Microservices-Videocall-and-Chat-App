a01ab2acbc4e36c8eca9320eb7c2aa8a397ba4f4
Sebastian Ślimak <sebastian.slimak707@gmail.com>
2023-02-27 00:13:53 +0100

    deploy.sh changed

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,17 @@
 eval $(minikube docker-env)
-docker build -f backend/group-service/Dockerfile -t chat/groupservice .
-docker build -f backend/token-service/Dockerfile -t chat/tokenservice .
-docker build -f backend/search-service/Dockerfile -t chat/searchservice .
-docker build -f backend/user-service/Dockerfile -t chat/userservice .
-docker build -f backend/ws-service/Dockerfile -t chat/wsservice .
-docker build -f backend/message-service/Dockerfile -t chat/messageservice .
+docker build -f backend/group-service/Dockerfile --build-arg configFile=config/kube-conf.yaml -t chat/groupservice .
+docker build -f backend/token-service/Dockerfile --build-arg configFile=config/kube-conf.yaml -t chat/tokenservice .
+docker build -f backend/search-service/Dockerfile --build-arg configFile=config/kube-conf.yaml -t chat/searchservice .
+docker build -f backend/user-service/Dockerfile --build-arg configFile=config/kube-conf.yaml -t chat/userservice .
+docker build -f backend/ws-service/Dockerfile --build-arg configFile=config/kube-conf.yaml -t chat/wsservice .
+docker build -f backend/message-service/Dockerfile --build-arg configFile=config/kube-conf.yaml -t chat/messageservice .
 docker build -f frontend/Dockerfile -t chat/frontend .
 docker image rm $(docker images -f "dangling=true" -q)
+
+helm install kafka bitnami/kafka
+
+kubectl apply -f deploy/secrets
+kubectl apply -f deploy/dbs
+kubectl apply -f deploy/services
+kubectl apply -f deploy/frontend.yaml
+kubectl apply -f deploy/ingress.yaml
