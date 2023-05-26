3ed8c950e685279299d7e7928a2b73d07550d6f5
Slimo300 <sebastian.slimak707@gmail.com>
2023-05-26 23:38:20 +0200

    ServiceID field added to messageSentEvent

--- backend/lib/events/message_sent.go
+++ backend/lib/events/message_sent.go
@@ -8,13 +8,14 @@ import (
 
 // MessageSentEvent holds information about message being sent
 type MessageSentEvent struct {
-	ID      uuid.UUID `json:"messageID" mapstructure:"messageID"`
-	GroupID uuid.UUID `json:"groupID" mapstructure:"groupID"`
-	UserID  uuid.UUID `json:"userID" mapstructure:"userID"`
-	Text    string    `json:"text" mapstructure:"text"`
-	Nick    string    `json:"nick" mapstructure:"nick"`
-	Posted  time.Time `json:"posted" mapstructure:"posted"`
-	Files   []File    `json:"files" mapstructure:"files"`
+	ID        uuid.UUID `json:"messageID" mapstructure:"messageID"`
+	GroupID   uuid.UUID `json:"groupID" mapstructure:"groupID"`
+	UserID    uuid.UUID `json:"userID" mapstructure:"userID"`
+	Text      string    `json:"text" mapstructure:"text"`
+	Nick      string    `json:"nick" mapstructure:"nick"`
+	Posted    time.Time `json:"posted" mapstructure:"posted"`
+	Files     []File    `json:"files" mapstructure:"files"`
+	ServiceID uuid.UUID `json:"serviceID" mapstructure:"serviceID"`
 }
 
 // File holds information about file send alongside message
