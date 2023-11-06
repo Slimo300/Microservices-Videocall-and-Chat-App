70d017f1206e67a8ace7dab3c2803f1b957d6562
Slimo300 <sebastian.slimak707@gmail.com>
2023-11-06 21:04:03 +0100

    added muting to member events

--- backend/lib/events/member_created.go
+++ backend/lib/events/member_created.go
@@ -6,15 +6,11 @@ import (
 
 // MemberCreatedEvent holds information about adding user to a group
 type MemberCreatedEvent struct {
-	ID               uuid.UUID `json:"ID" mapstructure:"ID"`
-	GroupID          uuid.UUID `json:"groupID" mapstructure:"groupID"`
-	UserID           uuid.UUID `json:"userID" mapstructure:"userID"`
-	User             User      `json:"User" mapstructure:"User"`
-	Adding           bool      `json:"adding" mapstructure:"adding"`
-	DeletingMembers  bool      `json:"deletingMembers" mapstructure:"deletingMembers"`
-	DeletingMessages bool      `json:"deletingMessages" mapstructure:"deletingMessages"`
-	Admin            bool      `json:"admin" mapstructure:"admin"`
-	Creator          bool      `json:"creator" mapstructure:"creator"`
+	ID      uuid.UUID `json:"ID" mapstructure:"ID"`
+	GroupID uuid.UUID `json:"groupID" mapstructure:"groupID"`
+	UserID  uuid.UUID `json:"userID" mapstructure:"userID"`
+	User    User      `json:"User" mapstructure:"User"`
+	Creator bool      `json:"creator" mapstructure:"creator"`
 }
 
 // EventName method from Event interface
