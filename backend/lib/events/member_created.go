d4d0e123fc317a1cf5c7a22604a572d8825a5007
Slimo300 <sebastian.slimak707@gmail.com>
2023-11-06 22:44:05 +0100

    restoring membercreatedevent

--- backend/lib/events/member_created.go
+++ backend/lib/events/member_created.go
@@ -6,11 +6,16 @@ import (
 
 // MemberCreatedEvent holds information about adding user to a group
 type MemberCreatedEvent struct {
-	ID      uuid.UUID `json:"ID" mapstructure:"ID"`
-	GroupID uuid.UUID `json:"groupID" mapstructure:"groupID"`
-	UserID  uuid.UUID `json:"userID" mapstructure:"userID"`
-	User    User      `json:"User" mapstructure:"User"`
-	Creator bool      `json:"creator" mapstructure:"creator"`
+	ID               uuid.UUID `json:"ID" mapstructure:"ID"`
+	GroupID          uuid.UUID `json:"groupID" mapstructure:"groupID"`
+	UserID           uuid.UUID `json:"userID" mapstructure:"userID"`
+	User             User      `json:"User" mapstructure:"User"`
+	Creator          bool      `json:"creator" mapstructure:"creator"`
+	Admin            bool      `json:"admin" mapstructure:"admin"`
+	Adding           bool      `json:"adding" mapstructure:"adding"`
+	DeletingMembers  bool      `json:"deletingMembers" mapstructure:"deletingMembers"`
+	DeletingMessages bool      `json:"deletingMessages" mapstructure:"deletingMessages"`
+	Muting           bool      `json:"muting" mapstructure:"muting"`
 }
 
 // EventName method from Event interface
