8a242c1b234765c5421c0a6dc43583a3a1d43e59
Slimo300 <sebastian.slimak707@gmail.com>
2023-11-07 13:35:27 +0100

    messagesentevent must have a memberID

--- backend/lib/events/message_sent.go
+++ backend/lib/events/message_sent.go
@@ -9,6 +9,7 @@ import (
 // MessageSentEvent holds information about message being sent
 type MessageSentEvent struct {
 	ID        uuid.UUID `json:"messageID" mapstructure:"messageID"`
+	MemberID  uuid.UUID `json:"memberID" mapstructure:"memberID"`
 	GroupID   uuid.UUID `json:"groupID" mapstructure:"groupID"`
 	UserID    uuid.UUID `json:"userID" mapstructure:"userID"`
 	Text      string    `json:"text" mapstructure:"text"`
